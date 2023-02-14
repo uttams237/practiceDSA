1242cdaa5fa3e92d2e9581d11de20b6047a4dbda
Uttam Kumar Sharma <64878251+uttams237@users.noreply.github.com>
2023-02-14 17:11:55 +0530

    Create pbds.cpp

---
+++ Algorithms/pbds.cpp
@@ -0,0 +1,4 @@
+using namespace __gnu_pbds;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pbds tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
